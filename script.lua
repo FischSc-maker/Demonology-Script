6da579cb40d02147b5666c34f3d3e56c3a22a8b7
FischSc-maker <afernandoveiga@gmail.com>
2025-01-26 15:23:15 -0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -7,62 +7,229 @@
  Y888P  ~Y8888P' Y888888P      888888D      Y88888P ~Y8888P' YP   YP  CONVERTER 
 ]=]
 
--- Instances: 5 | Scripts: 1 | Modules: 0 | Tags: 0
+-- Instances: 16 | Scripts: 2 | Modules: 0 | Tags: 0
 local G2L = {};
 
--- StarterGui.Yes
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk
 G2L["1"] = Instance.new("ScreenGui", game:GetService("CoreGui"));
-G2L["1"]["IgnoreGuiInset"] = true;
-G2L["1"]["ScreenInsets"] = Enum.ScreenInsets.DeviceSafeInsets;
-G2L["1"]["Name"] = [[Yes]];
+G2L["1"]["Name"] = [[habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk]];
 G2L["1"]["ZIndexBehavior"] = Enum.ZIndexBehavior.Sibling;
 
 
--- StarterGui.Yes.Frame
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame
 G2L["2"] = Instance.new("Frame", G2L["1"]);
 G2L["2"]["BorderSizePixel"] = 0;
 G2L["2"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
-G2L["2"]["Size"] = UDim2.new(0.41074, 0, 0.21322, 0);
-G2L["2"]["Position"] = UDim2.new(0.27715, 0, 0.371, 0);
+G2L["2"]["Size"] = UDim2.new(0, 375, 0, 21);
+G2L["2"]["Position"] = UDim2.new(0.10612, 0, 0.1791, 0);
 G2L["2"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["2"]["BackgroundTransparency"] = 1;
 
 
--- StarterGui.Yes.Frame.Ghost type
-G2L["3"] = Instance.new("TextLabel", G2L["2"]);
-G2L["3"]["TextWrapped"] = true;
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame
+G2L["3"] = Instance.new("Frame", G2L["2"]);
 G2L["3"]["BorderSizePixel"] = 0;
-G2L["3"]["TextSize"] = 14;
-G2L["3"]["TextScaled"] = true;
-G2L["3"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
-G2L["3"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
-G2L["3"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["3"]["Size"] = UDim2.new(0.6079, 0, 0.5, 0);
+G2L["3"]["BackgroundColor3"] = Color3.fromRGB(39, 23, 66);
+G2L["3"]["Size"] = UDim2.new(1.00071, 0, 9.86354, 0);
+G2L["3"]["Position"] = UDim2.new(0, 0, 0.17911, 0);
 G2L["3"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
-G2L["3"]["Name"] = [[Ghost type]];
-G2L["3"]["Position"] = UDim2.new(0.19453, 0, 0.25, 0);
 
 
--- StarterGui.Yes.Frame.Ghost type.LocalScript
-G2L["4"] = Instance.new("LocalScript", G2L["3"]);
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.UICorner
+G2L["4"] = Instance.new("UICorner", G2L["3"]);
 
 
 
--- StarterGui.Yes.Frame.Ghost type.UITextSizeConstraint
-G2L["5"] = Instance.new("UITextSizeConstraint", G2L["3"]);
-G2L["5"]["MaxTextSize"] = 15;
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.UIStroke
+G2L["5"] = Instance.new("UIStroke", G2L["3"]);
 
 
--- StarterGui.Yes.Frame.Ghost type.LocalScript
-local function C_4()
-local script = G2L["4"];
-	local ghosttype = workspace.Ghost:GetAttribute("GhostType")
-	if type(ghosttype) ~= "string" then
-		tostring(ghosttype)
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.Title
+G2L["6"] = Instance.new("TextLabel", G2L["3"]);
+G2L["6"]["TextWrapped"] = true;
+G2L["6"]["BorderSizePixel"] = 0;
+G2L["6"]["TextSize"] = 14;
+G2L["6"]["TextScaled"] = true;
+G2L["6"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["6"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Bold, Enum.FontStyle.Italic);
+G2L["6"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["6"]["BackgroundTransparency"] = 1;
+G2L["6"]["Size"] = UDim2.new(0.38138, 0, 0.10294, 0);
+G2L["6"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["6"]["Text"] = [[Fer Modz - Demonology]];
+G2L["6"]["Name"] = [[Title]];
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.Title.UITextSizeConstraint
+G2L["7"] = Instance.new("UITextSizeConstraint", G2L["6"]);
+G2L["7"]["MaxTextSize"] = 15;
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.Close
+G2L["8"] = Instance.new("TextButton", G2L["3"]);
+G2L["8"]["BorderSizePixel"] = 0;
+G2L["8"]["TextColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["TextSize"] = 14;
+G2L["8"]["BackgroundColor3"] = Color3.fromRGB(255, 0, 0);
+G2L["8"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal);
+G2L["8"]["Size"] = UDim2.new(0.10511, 0, 0.12255, 0);
+G2L["8"]["Name"] = [[Close]];
+G2L["8"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["8"]["Text"] = [[X]];
+G2L["8"]["Position"] = UDim2.new(0.97898, 0, -0.07353, 0);
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.Close.UIStroke
+G2L["9"] = Instance.new("UIStroke", G2L["8"]);
+
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.Close.UICorner
+G2L["a"] = Instance.new("UICorner", G2L["8"]);
+
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.GType
+G2L["b"] = Instance.new("TextLabel", G2L["3"]);
+G2L["b"]["TextWrapped"] = true;
+G2L["b"]["BorderSizePixel"] = 0;
+G2L["b"]["TextSize"] = 14;
+G2L["b"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["b"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Bold, Enum.FontStyle.Italic);
+G2L["b"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["b"]["BackgroundTransparency"] = 1;
+G2L["b"]["RichText"] = true;
+G2L["b"]["Size"] = UDim2.new(0.46101, 0, 0.24139, 0);
+G2L["b"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["b"]["Text"] = [[Ghost Type :]];
+G2L["b"]["Name"] = [[GType]];
+G2L["b"]["Position"] = UDim2.new(0.02132, 0, 0.13518, 0);
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.GEsp
+G2L["c"] = Instance.new("TextButton", G2L["3"]);
+G2L["c"]["BorderSizePixel"] = 0;
+G2L["c"]["TextColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["c"]["TextSize"] = 14;
+G2L["c"]["BackgroundColor3"] = Color3.fromRGB(255, 255, 255);
+G2L["c"]["FontFace"] = Font.new([[rbxasset://fonts/families/SourceSansPro.json]], Enum.FontWeight.Bold, Enum.FontStyle.Italic);
+G2L["c"]["Size"] = UDim2.new(0, 173, 0, 49);
+G2L["c"]["BackgroundTransparency"] = 1;
+G2L["c"]["Name"] = [[GEsp]];
+G2L["c"]["BorderColor3"] = Color3.fromRGB(0, 0, 0);
+G2L["c"]["Text"] = [[Ghost Esp]];
+G2L["c"]["Position"] = UDim2.new(0.02132, 0, 0.37657, 0);
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.GEsp.UIStroke
+G2L["d"] = Instance.new("UIStroke", G2L["c"]);
+G2L["d"]["ApplyStrokeMode"] = Enum.ApplyStrokeMode.Border;
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.GEsp.UICorner
+G2L["e"] = Instance.new("UICorner", G2L["c"]);
+
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.Main
+G2L["f"] = Instance.new("LocalScript", G2L["3"]);
+G2L["f"]["Name"] = [[Main]];
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.UIDrag
+G2L["10"] = Instance.new("LocalScript", G2L["2"]);
+G2L["10"]["Name"] = [[UIDrag]];
+
+
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.Frame.Main
+local function C_f()
+local script = G2L["f"];
+	local gfound = false
+	
+	if workspace:FindFirstChild("Ghost") then
+		gfound = true
 	end
-	script.Parent.Text = ghosttype
-	wait(5.55)
-	script.Parent.Parent.Parent:Destroy()
+	
+	if gfound == false then
+		game.Players.LocalPlayer:Kick("Ghost Not found.")
+	end
+	
+	local ghtype = workspace.Ghost:GetAttribute("GhostType")
+	
+	local fr = script.Parent
+	
+	local cls = fr.Close
+	local gesp = fr.GEsp
+	local gtype = fr.GType
+	
+	cls.MouseButton1Click:Connect(function() 
+		if workspace:FindFirstChild("Part") then
+			if workspace.Part:FindFirstChild("Highlight") then
+				workspace.Part:FindFirstChild("Highlight"):Destroy()
+			end
+		end
+		fr.Parent.Parent:Destroy()
+	end)
+	
+	gesp.MouseButton1Click:Connect(function() 
+		local part = Instance.new("Part")
+		part.Parent = workspace
+		Instance.new("Highlight", part)
+		part.Size = workspace.Ghost.HumanoidRootPart.Size
+		while task.wait() do
+			part.CFrame = workspace.Ghost.HumanoidRootPart.CFrame
+			part.CanCollide = false
+		end
+	end)
+	
+	gtype.Text = "Ghost Type : "..ghtype
+	
+	
+	game:GetService("ReplicatedStorage").Events.EvidenceMarkedInJournal:FireServer(ghtype)
 end;
-task.spawn(C_4);
+task.spawn(C_f);
+-- StarterGui.habkdnsjmjsskxksxmksmkmkkxskskmxkmxksk.Frame.UIDrag
+local function C_10()
+local script = G2L["10"];
+	-- Made by Real_IceyDev (@lceyDex) --
+	-- Simple UI dragger (PC Only/Any device that has a mouse) --
+	
+	local UIS = game:GetService('UserInputService')
+	local frame = script.Parent
+	local dragToggle = nil
+	local dragSpeed = 0.25
+	local dragStart = nil
+	local startPos = nil
+	
+	local function updateInput(input)
+		local delta = input.Position - dragStart
+		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
+			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	end
+	
+	frame.InputBegan:Connect(function(input)
+		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+			dragToggle = true
+			dragStart = input.Position
+			startPos = frame.Position
+			input.Changed:Connect(function()
+				if input.UserInputState == Enum.UserInputState.End then
+					dragToggle = false
+				end
+			end)
+		end
+	end)
+	
+	UIS.InputChanged:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
+			if dragToggle then
+				updateInput(input)
+			end
+		end
+	end)
+end;
+task.spawn(C_10);
 
 return G2L["1"], require;
